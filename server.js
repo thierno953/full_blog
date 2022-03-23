42f9c6d34fa57e37cf1ae2b48635ae535782d2a6
Thierno Barry <thiernobarry554@gmail.com>
2022-03-23 11:37:56 +0100

    JSON Server

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-const express = require("express");
-const path = require("path");
-const app = express();
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./db.json");
+const middlewares = jsonServer.defaults({
+  static: "./build",
+});
 
-app.use(express.static(path.join(__dirname, "./build")))
+const port = process.env.PORT || 8000;
+server.use(middlewares);
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "./build/index.html"))
-}) 
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./build/index.html"))
-})
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
+server.use(router);
+server.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
--- src/components/utils.js
+++ src/components/utils.js
@@ -1,1 +1,1 @@
-export const JSON_API = 'http://localhost:5000'
+export const JSON_API = 'http://localhost:4000'
