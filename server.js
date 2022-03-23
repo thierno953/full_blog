e35596b0c60caf654d185f97eae576f1fdaeea80
Thierno Barry <thiernobarry554@gmail.com>
2022-03-23 10:50:47 +0100

    JSON Server

--- server.js
+++ server.js
@@ -6,14 +6,10 @@ const middlewares = jsonServer.defaults({
 });
 
 const port = process.env.PORT || 5000;
-server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "$1",
-  })
-);
 
+server.use(middlewares);
 server.use(router);
+
 server.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
