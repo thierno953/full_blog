5e04745f9ea0c80026866dc5ab066a99cc417670
Thierno Barry <thiernobarry554@gmail.com>
2022-03-23 11:16:54 +0100

    JSON Server

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
-const middlewares = jsonServer.defaults({
-  static: "./build",
-});
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "./build")))
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./build/index.html"))
+}) 
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./build/index.html"))
+})
 
 const port = process.env.PORT || 5000;
 
-server.use(middlewares);
-server.use(router);
-
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
